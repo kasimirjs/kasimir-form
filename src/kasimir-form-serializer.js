522e63e6cba57f692dc52063b45de29c99edf7a3
Matthes <matthes@leuffen.de>
2019-06-28 23:28:36 +0200

    setup

---
+++ src/kasimir-form-serializer.js
@@ -0,0 +1,20 @@
+
+class KasimirFormSerializer {
+
+    static GetElemValue (formSelector) {
+
+    }
+
+    static SetElemValue (formSelector, newValue) {
+
+    }
+
+    static GetData(formSelector) {
+
+    }
+
+    static SetData(formSelector, newValue) {
+
+    }
+
+}
---
+++ src/kasimir-form.js
@@ -0,0 +1,16 @@
+
+class KasimirForm {
+
+
+    constructor(selector) {
+
+    }
+
+    bind(object) {
+
+        return this;
+    }
+
+
+
+}
