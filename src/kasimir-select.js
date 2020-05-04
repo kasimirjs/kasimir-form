56f097d55bac3202e1411b444154b85885f228ee
Matthes <matthes@leuffen.de>
2020-05-05 00:19:11 +0200

    added select

--- src/kasimir-select.js
+++ src/kasimir-select.js
@@ -1,5 +1,61 @@
 
-class KasimirSelect {
+class KasimirSelect extends HTMLSelectElement {
+
+
+    constructor() {
+        super();
+        this.__$options = [];
+    }
+
+
+    _updateOptions() {
+        console.log("updateOptions()");
+        this.innerHTML = "";
+        for(let option of this.__$options) {
+            let optEl = document.createElement("option");
+            if (typeof option === "object") {
+                optEl.value = option.value;
+                optEl.innerText = option.text;
+            } else {
+                optEl.value = option;
+                optEl.innerText = option;
+            }
+            this.appendChild(optEl);
+        }
+    }
+
+
+    connectedCallback() {
+        let iniOptions = this.$options;
+        let value = this.$value;
+
+        // Getters / Setters not possible if property already defined.
+        // This happens if element is loaded before js
+        // Therefor: apply only on connect and keep the property value
+        Object.defineProperty(this, '$options', {
+            set: (val) => {
+                this.__$options = val;
+                this._updateOptions();
+            },
+            get: (val) => {
+                return this.__$options
+            }
+        });
+        Object.defineProperty(this, '$value', {
+            set: (val) => {
+                this.value = val;
+            },
+            get: (val) => {
+                return this.value;
+            }
+        });
+        if (typeof iniOptions !== "undefined")
+            this.$options = iniOptions;
+        if (typeof value !== "undefined")
+            this.$value = value;
+    }
+
 
 }
 
+customElements.define("ka-select", KasimirSelect, {extends: "select"});
